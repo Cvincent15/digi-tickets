d09cb0547e79f8f4d39492e9165d4dc1efa8930a
Cvincent15 <116704777+Cvincent15@users.noreply.github.com>
2023-09-09 00:25:46 +0800

    Add files via upload

--- php/editDetails.php
+++ php/editDetails.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'database_connect.php'; // Include your database connection code here
+include 'database_connect.php';
 
 // Check if the user is already logged in
 if (!isset($_SESSION['username'])) {
@@ -10,20 +10,26 @@ if (!isset($_SESSION['username'])) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form data and sanitize it
-    $driverName = mysqli_real_escape_string($conn, $_POST['driver_name']);
-    $driverAddress = mysqli_real_escape_string($conn, $_POST['driver_address']);
-    $driverLicense = mysqli_real_escape_string($conn, $_POST['driver_license']);
-    $issuingDistrict = mysqli_real_escape_string($conn, $_POST['issuing_district']);
-    $vehicleType = mysqli_real_escape_string($conn, $_POST['vehicle_type']);
-    $plateNo = mysqli_real_escape_string($conn, $_POST['plate_no']);
-    $corNo = isset($_POST['cor_no']) ? mysqli_real_escape_string($conn, $_POST['cor_no']) : null;
-    $placeIssued = isset($_POST['place_issued']) ? mysqli_real_escape_string($conn, $_POST['place_issued']) : null;
-    $regOwner = mysqli_real_escape_string($conn, $_POST['reg_owner']);
-    $regOwnerAddress = mysqli_real_escape_string($conn, $_POST['reg_owner_address']);
-    $dateTimeViolation = mysqli_real_escape_string($conn, $_POST['date_time_violation']);
-    $placeOfOccurrence = mysqli_real_escape_string($conn, $_POST['place_of_occurrence']);
+    // Get the form data and sanitize/validate it
+    $driverName = filter_input(INPUT_POST, 'driver_name', FILTER_SANITIZE_STRING);
+    $driverAddress = filter_input(INPUT_POST, 'driver_address', FILTER_SANITIZE_STRING);
+    $driverLicense = filter_input(INPUT_POST, 'driver_license', FILTER_SANITIZE_STRING);
+    $issuingDistrict = filter_input(INPUT_POST, 'issuing_district', FILTER_SANITIZE_STRING);
+    $vehicleType = filter_input(INPUT_POST, 'vehicle_type', FILTER_SANITIZE_STRING);
+    $plateNo = filter_input(INPUT_POST, 'plate_no', FILTER_SANITIZE_STRING);
+    $corNo = isset($_POST['cor_no']) ? filter_input(INPUT_POST, 'cor_no', FILTER_SANITIZE_STRING) : null;
+    $placeIssued = isset($_POST['place_issued']) ? filter_input(INPUT_POST, 'place_issued', FILTER_SANITIZE_STRING) : null;
+    $regOwner = filter_input(INPUT_POST, 'reg_owner', FILTER_SANITIZE_STRING);
+    $regOwnerAddress = filter_input(INPUT_POST, 'reg_owner_address', FILTER_SANITIZE_STRING);
+    $dateTimeViolation = filter_input(INPUT_POST, 'date_time_violation', FILTER_SANITIZE_STRING);
+    $placeOfOccurrence = filter_input(INPUT_POST, 'place_of_occurrence', FILTER_SANITIZE_STRING);
     $ticketId = intval($_POST['ticket_id']); // Convert to an integer
+
+    // Validate if necessary (e.g., minimum length for specific fields)
+    if (strlen($driverName) < 2 || strlen($driverLicense) < 2) {
+        echo "InvalidInput";
+        exit();
+    }
 
     // Prepare an SQL statement using prepared statements to update the user data
     $sql = "UPDATE violation_tickets 
@@ -38,50 +44,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 date_time_violation = ?,
                 place_of_occurrence = ?";
 
-    // Only add cor_no and place_issued to the SQL statement if they are not empty
-    if (!empty($corNo)) {
-        $sql .= ", cor_no = ?";
-    }
-    if (!empty($placeIssued)) {
-        $sql .= ", place_issued = ?";
-    }
-
-    // Add a WHERE clause to specify which user to update based on the ticket_id
-    $sql .= " WHERE ticket_id = ?";
-
     // Prepare the statement
-    $stmt = $conn->prepare($sql);
+$stmt = $conn->prepare($sql);
 
-    if ($stmt) {
-        // Bind the parameters to the prepared statement
-        $stmt->bind_param("ssssssssssi",
-            $driverName,
-            $driverAddress,
-            $driverLicense,
-            $issuingDistrict,
-            $vehicleType,
-            $plateNo,
-            $regOwner,
-            $regOwnerAddress,
-            $dateTimeViolation,
-            $placeOfOccurrence,
-            $ticketId
-        );
+if ($stmt) {
+    // Define all parameters and their types
+    $paramTypes = "ssssssssssii"; // Base types for all parameters
+    $params = [
+        &$driverName,
+        &$driverAddress,
+        &$driverLicense,
+        &$issuingDistrict,
+        &$vehicleType,
+        &$plateNo,
+        &$regOwner,
+        &$regOwnerAddress,
+        &$dateTimeViolation,
+        &$placeOfOccurrence,
+        &$ticketId,
+    ];
 
-        // Execute the SQL query
-        if ($stmt->execute()) {
-            // The update was successful
-            // Redirect to a success page or back to the details page
-            header("Location: ../ctmeupage.php");
-            exit();
-        } else {
-            // Handle the error, e.g., display an error message
-            echo "Error: " . $stmt->error;
-        }
-
-        // Close the prepared statement
-        $stmt->close();
+    // Conditionally add cor_no and place_issued to the parameters and types
+    if (!empty($corNo)) {
+        $paramTypes .= "s";
+        $params[] = &$corNo;
     }
+
+    if (!empty($placeIssued)) {
+        $paramTypes .= "s";
+        $params[] = &$placeIssued;
+    }
+
+    // Use call_user_func_array to bind parameters dynamically
+    $bindParams = array_merge([$paramTypes], ...$params);
+    call_user_func_array([$stmt, 'bind_param'], $bindParams);
+
+    // Execute the SQL query
+    if ($stmt->execute()) {
+        // The update was successful
+        // Redirect to a success page or back to the details page
+        header("Location: ../ctmeupage.php");
+        exit();
+    } else {
+        // Handle the error, e.g., display an error message
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the prepared statement
+    $stmt->close();
+}
+
 } else {
     // If the request is not a POST request, redirect to an error page or handle it accordingly
     echo "Error: Invalid request method";
